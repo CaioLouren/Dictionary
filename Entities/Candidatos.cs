eb50b10997c3a362e6671050acf2ec919f967af5
Caio Lourenço <lourencocaio228@gmail.com>
2022-06-29 16:58:33 -0300

    Atividade

---
+++ Entities/Candidatos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Candidatos
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public Candidatos(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> NumeroDeVotos = new Dictionary<string, int>();
+
+            Console.WriteLine("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int number = int.Parse(line[1]);
+
+                        if (!NumeroDeVotos.ContainsKey(name))
+                        {
+                            NumeroDeVotos.Add(name, number);
+                        }
+                        else
+                        {
+                            NumeroDeVotos[name] += number;
+                        }
+                       
+                    }
+                    foreach (KeyValuePair<string, int> item in NumeroDeVotos)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
